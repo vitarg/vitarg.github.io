dc6beca7aa9025c137bf148135e18f267d2ea849
vitarg <vitrigovm@gmail.com>
2021-02-01 12:26:06 +0300

    optimization uber

--- src/js/uber/script.js
+++ src/js/uber/script.js
@@ -3,8 +3,8 @@ let menuBtn = document.querySelector('.u-humburger')
 function humburger() {
     let menu = document.querySelector('.u-menu')
 
-    menu.classList.toggle('u-menu__active')
-    menuBtn.classList.toggle('u-humburger_active')
+    menu.classList.toggle('.u-menu_active')
+    menuBtn.classList.toggle('.u-humburger_active')
 }
 
 menuBtn.addEventListener('click', humburger)
