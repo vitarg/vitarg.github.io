ffc966e1513c5eb4a4f7dc9432f49692b9d251cc
vitarg <vitrigovm@gmail.com>
2021-02-02 09:46:06 +0300

    optimization uber

--- src/js/uber/script.js
+++ src/js/uber/script.js
@@ -1,10 +1,9 @@
-let menuBtn = document.querySelector('.u-humburger')
+let menuBtn = document.querySelector(".u-humburger");
+let menu = document.querySelector(".u-menu__list");
 
 function humburger() {
-    let menu = document.querySelector('.u-menu')
-
-    menu.classList.toggle('.u-menu_active')
-    menuBtn.classList.toggle('.u-humburger_active')
+    menu.classList.toggle("u-menu__list_active");
+    menuBtn.classList.toggle("u-humburger_active");
 }
 
-menuBtn.addEventListener('click', humburger)
+menuBtn.addEventListener("click", humburger);
