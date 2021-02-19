ceca35ba0ad000bea0952a8d29c1c10a0d7a014c
vitarg <vitrigovm@gmail.com>
2021-02-19 18:41:19 +0300

    update mk95

--- src/js/mk95/script.js
+++ src/js/mk95/script.js
@@ -7,3 +7,13 @@ function humburger() {
 }
 
 menuBtn.addEventListener("click", humburger);
+
+let singIn = document.querySelector(".m-header__sign-in-icon");
+let overlay = document.querySelector(".overlay");
+
+
+singIn.addEventListener("click", openModal);
+
+function openModal() {
+    overlay.classList.toggle("overlay_active");
+}
