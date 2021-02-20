43a9f60a0c4cb8bad78f0f7da6d11e9c210e47b6
vitarg <vitrigovm@gmail.com>
2021-02-20 08:00:38 +0300

    update pulse

---
+++ src/js/pulse/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".carousel__inner").slick({
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/pulse/left-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/pulse/right-arrow.png"></button>',
+    });
+});
