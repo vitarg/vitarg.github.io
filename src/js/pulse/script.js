6b61f0b56fcebec5d4e049a129ae8d3f68b1adcf
vitarg <vitrigovm@gmail.com>
2021-02-22 13:07:58 +0300

    update pulse

--- src/js/pulse/script.js
+++ src/js/pulse/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".carousel__inner").slick({
-        adaptiveHeight: true,
+        /* adaptiveHeight: true, */
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/pulse/left-arrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/pulse/right-arrow.png"></button>',
     });
